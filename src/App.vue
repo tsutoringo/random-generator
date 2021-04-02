7aa661f5e8d1ef4a305aebade3d26429d8ac4965
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-04-02 10:01:01 +0900

    読み上げ機能追加とテンプレート追加

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 		<textarea v-model="randoms" cols="30" rows="10"></textarea><br>
 		結果:<br>
 		<textarea v-model="result" cols="30" rows="10"></textarea><br>
-		<button @click="generate()">生成</button>
+		<button @click="generate()">生成</button><button @click="tts()">読み上げ</button>
 		<details>
 			<summary>プライバシーポリシー</summary>
 			このサイトは Google アナリティクスを使用しています
@@ -27,8 +27,14 @@ export default {
 		result: "",
 		templates: [
 			{
-				name: 'エヴァンゲリオン',
+				name: 'ミサトさん',
 				a: 'シンジくん！\nシンジくん！？\n聞いてる？\nこれが大人のキスよ\nわかったの！？\nいい！？\nエヴァーに乗りなさい\nうるさい！\n死になさい！\nサードチルドレンのあなたは'
+			}, {
+				name: '冨岡義勇',
+				a: '生殺与奪の\n権を\n他人に\n握らせるな！\n みじめったらしく\nうずくまるのは\nやめろ！ \nそんなことが\n通用する\nなら \nお前の\n家族は\n殺されて\nいない！ \n奪うか\n奪われるかの\n時に\n主導権を\n握れない\n弱者が\n妹を治す？\n仇を見つける？\n笑止千万！！\n弱者には\n何の権利も\n選択肢もない\n悉く\n強者に\nねじ伏せられる\nのみ！！\n妹を\n治す方法は\n鬼なら\n知っているかもしれない\nだが\n鬼共が\nお前の\n意思や\n願いを\n尊重してくれると\n思うなよ\n当然\n俺も\nお前を\n尊重しない\nそれが現実だ！！！'
+			}, {
+				name: 'アマゾンの奥地へ向かった探検隊',
+				a: '何かしら\n理由が\nあると\n皆が\n目指し\n始める\nアマゾンの\n奥地には\n一体何が\nあるのか？\nその謎を\n解明するため、\n我々調査隊は\nアマゾンの\n奥地へと\n向かった――。'
 			}, {
 				name: 'ピエンｗ',
 				a: 'ﾋﾟ\nｴ\nﾝ\n🥺\nw'
@@ -47,6 +53,11 @@ export default {
 			for (let i=0; i<this.count; i++) {
 				this.result += randoms[Math.floor(Math.random() * randoms.length)]
 			}
+		},
+		tts () {
+			const msg = new SpeechSynthesisUtterance();
+			msg.text = this.result;
+			window.speechSynthesis.speak(msg);
 		}
 	}
 }
